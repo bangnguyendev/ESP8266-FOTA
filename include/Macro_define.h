dff535c2e0a5ec16aaf83f1307874099b4d33ab6
bangnguyendev <duybang140494@gmail.com>
2024-04-03 16:39:17 +0700

    Init project ESP8266 FOTA Ver1.0.0

--- include/Macro_define.h
+++ include/Macro_define.h
@@ -1,6 +1,6 @@
 /* Cập nhật OTA */
 
-const String FirmwareVer={"v1.0.9"}; 
+const String FirmwareVer={"v1.0.0"}; 
 
 #define URL_fw_Version "/bangnguyendev/ESP8266-FOTA/master/include/Info_prod.json"
 #define URL_fw_Bin "https://raw.githubusercontent.com/bangnguyendev/ESP8266-FOTA/master/build/main.ino.bin"
